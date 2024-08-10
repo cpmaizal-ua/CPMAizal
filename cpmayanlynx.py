2a8210dd407b773f9bd8694541ee63e7702fc552
Cpmayan <bdbdgdvddv@gmail.com>
2024-08-10 12:53:55 +0500

    Update cpmayanlynx.py

--- cpmayanlynx.py
+++ cpmayanlynx.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://cpmnuker.anasov.ly/api"
 
-class CPMNuker:
+class CPMAYANLYNX:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
