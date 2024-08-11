84ddf01d26ffd51deec83648ba92ae2458fc66c4
Cpmayan <bdbdgdvddv@gmail.com>
2024-08-11 19:53:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
-from cpmnuker import CPMNuker
 
 __CHANNEL_USERNAME__ = "CPMAyan"
 __GROUP_USERNAME__   = "CPMAyanChat"
