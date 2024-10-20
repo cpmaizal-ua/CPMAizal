85c7f9437b4bc618c493591478fb1094dad7620b
CPMAizal <bdbdgdvddv@gmail.com>
2024-10-20 07:46:52 +0500

    Update cpmgala.py

--- cpmgala.py
+++ cpmgala.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-BASE_URL: str = "https://www.cpmewan.com/api"
+BASE_URL: str = "https://red-viper-815239.hostingersite.com/api"
 
-class CPMEwan:
+class CPMGala:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
