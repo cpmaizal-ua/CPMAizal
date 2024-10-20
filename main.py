59dd80e348fcfd3df092a5f6115c96f68de6eeeb
CPMAizal <bdbdgdvddv@gmail.com>
2024-10-20 07:45:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmewan import CPMEwan
+from cpmgala import CPMGala
 
 __CHANNEL_USERNAME__ = "Ewan1999Ewan"
 __GROUP_USERNAME__   = "Ewan19_99Ewan"
